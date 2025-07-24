56c0efdbe255002ef3f92edf9b81737115d290e5
Mohaned-Adel1 <Mohanedadelelsayed@gmail.com>
2025-07-24 18:19:22 +0300

    add notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MainLayer from "./MainLayer/MainLayer";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import Notfound from "./Pages/NotFound";
 import Portfolio from "./Pages/Portfolio";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
         { path: "/about", element: <About /> },
         { path: "/Portfolio", element: <Portfolio /> },
         { path: "/Contact", element: <Contact /> },
+        { path: "*", element: <Notfound /> },
       ],
     },
   ]);
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,8 @@
+import notFound from "./../assets/notFound.jpg";
+export default function Notfound() {
+  return (
+    <div>
+      <img className=" w-100 " src={notFound} />
+    </div>
+  );
+}
